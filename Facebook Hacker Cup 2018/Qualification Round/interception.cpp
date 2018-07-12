ae99b020e45a4aa8ba548c64297bc5cc09948277
Abhimanyue Singh Tanwar <abhimanyu891998@users.noreply.github.com>
2018-07-12 01:21:06 +0100

    FB Hacker Cup 2018 QualInterception problem

---
+++ Facebook Hacker Cup 2018/Qualification Round/interception.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+
+	for(int i=1 ; i<=t ; i++)
+	{
+		int n;
+		cin>>n;
+		for(int j=0 ; j<=n ; j++)
+		{
+			int temp;
+			cin>>temp;
+		}
+
+		if(n%2==0)
+		{
+			cout<<"Case #"<<i<<":"<<" "<<"0"<<endl;
+		}
+		else
+		{
+			cout<<"Case #"<<i<<":"<<" "<<1<<endl;
+			cout<<"0.0"<<endl;
+		}
+	}
+
+
+}
