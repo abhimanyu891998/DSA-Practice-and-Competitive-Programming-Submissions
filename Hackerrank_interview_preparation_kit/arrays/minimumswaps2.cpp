856b69015013249e6f28c7d736d64acf253f3a35
Abhimanyue Singh Tanwar <abhimanyu891998@users.noreply.github.com>
2018-08-14 14:37:45 +0530

    Hackerrank Interview Preparation Kit

---
+++ Hackerrank_interview_preparation_kit/arrays/minimumswaps2.cpp
@@ -0,0 +1,109 @@
+//https://www.hackerrank.com/challenges/minimum-swaps-2/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the minimumSwaps function below.
+int minimumSwaps(vector<int> arr) {
+    int min_swaps = 0;
+    vector<int>temparr;
+    temparr = arr;
+    sort(arr.begin(),arr.end());
+
+    for(int i=0 ; i<arr.size() ; i++)
+    {
+        /*if(temparr[i]!=arr[i])
+        {   
+            int temp = temparr[i];
+            int x =  find(temparr.begin(), temparr.end(), arr[i]) - temparr.begin();
+            temparr[i] = arr[i];
+            temparr[x] = temp;
+
+
+
+
+           /* for(int j=i ; j<arr.size() ; j++)
+            {
+                if(temparr[j]==arr[i])
+                {
+                    temparr[j] = temp;
+                }
+            } */
+            
+
+            if(temparr[i]==arr[i])
+            continue;
+        
+            swap(temparr[i],temparr[temparr[i]-1]);
+        
+            i--;
+            min_swaps++;
+
+        }
+    
+  
+    return min_swaps;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int res = minimumSwaps(arr);
+
+    fout << res << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
