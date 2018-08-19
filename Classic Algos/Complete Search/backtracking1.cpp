f0818a139829f8b9647ee65be65b04105385428d
Abhimanyue Singh Tanwar <abhimanyu891998@users.noreply.github.com>
2018-08-20 01:25:29 +0530

    TwitterU, Classic Algos

---
+++ Classic Algos/Complete Search/backtracking1.cpp
@@ -0,0 +1,41 @@
+//Algorithm to find all number of ways of placing n queens on n*n chessboard using backtracking and recursion
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int n=0; int countways=0; vector<int>column = {0}; vector<int>diag1 = {0}; vector<int>diag2 = {0}; //Global vars
+
+void search(int y)
+{
+	if(y==n)
+	{
+		countways++;
+		return;
+	}
+
+	else
+	{
+		for(int x=0; x<n ; x++)
+		{
+			if(column[x]||diag1[x+y]||diag2[x-y+n-1])
+				continue;
+			else
+				column[x]=diag1[x+y]=diag2[x-y+n-1]=1;
+				search(y+1);
+				column[x]=diag1[x+y]=diag2[x-y+n-1]=0;
+		}
+	}
+
+	
+
+
+}
+
+int main()
+{
+   cin>>n;
+   search(0);
+   cout<<countways;
+
+   return 0;
+}
---
+++ TwitterUChallenge2019/problem1.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// Complete the decode function below.
+string decode(string encoded) {
+
+    string output = "";
+    reverse(encoded.begin(),encoded.end());
+    for(int i=0;i<encoded.length() ; i++)
+    {   
+        string asciiInt = "";
+        if(encoded[i]== '1')
+        {   
+        
+            asciiInt += encoded[i];
+            asciiInt += encoded[i+1];
+            asciiInt += encoded[i+2];
+            i+=2;
+        }
+        
+        else
+        {
+            asciiInt += encoded[i];
+            asciiInt += encoded[i+1];
+            i++;
+        }
+       
+        stringstream ss(asciiInt);
+        int asciiInteger=0;
+        ss>>asciiInteger;
+        
+       
+        output+= (char)asciiInteger;
+        
+    }
+    
+    return output;
+
+}
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string encoded;
+    getline(cin, encoded);
+
+    string res = decode(encoded);
+
+    fout << res << "\n";
+
+    fout.close();
+
+    return 0;
+}
