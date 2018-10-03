3e5f6727a90616f22939397e8c1e1c2217b9aaa6
Abhimanyue Singh Tanwar <abhimanyu891998@users.noreply.github.com>
2018-10-04 02:07:16 +0800

    Arrays

---
+++ InterviewBit/maxspprod.cpp
@@ -0,0 +1,67 @@
+//https://www.interviewbit.com/problems/maxspprod/
+vector<int> Solution::maxset(vector<int> &A) {
+    
+    long int currentSum=0;
+    long int maxSum =0;
+    vector<int> maxSumSubArray;
+    vector<int> currentSumSubArray;
+    for(int i=0 ; i<A.size() ; i++)
+    {  
+        
+       if((max(currentSum, currentSum+A[i]) == currentSum) && (currentSum!= currentSum+A[i]))
+       {   
+          if(currentSum==maxSum)
+          {
+              maxSum = currentSum;
+              maxSumSubArray.clear();
+              for(int j=0 ; j<currentSumSubArray.size() ; j++)
+              {
+                  maxSumSubArray.push_back(currentSumSubArray[j]);
+              }
+              
+          }
+          
+         currentSumSubArray.clear();
+         
+          currentSum=0;
+           
+           
+       }
+       
+       else
+       {   
+           
+            currentSum+= A[i];
+           currentSumSubArray.push_back(A[i]);
+           
+           if(maxSum<=currentSum)
+           {  
+               maxSum=currentSum;
+           }
+       }
+       
+    }
+    
+    if(currentSum == maxSum)
+    {
+        if(currentSumSubArray.size() >= maxSumSubArray.size())
+          {
+              maxSum = currentSum;
+              maxSumSubArray.clear();
+              for(int j=0 ; j<currentSumSubArray.size() ; j++)
+              {
+                  maxSumSubArray.push_back(currentSumSubArray[j]);
+              }
+              
+          }
+        
+    }
+    
+    //int maxSubArraySize = maxSumSubArray.size();
+    //maxSumSubArray.insert(maxSumSubArray.begin(),maxSubArraySize);
+    
+    return maxSumSubArray;
+    
+    
+     
+}
