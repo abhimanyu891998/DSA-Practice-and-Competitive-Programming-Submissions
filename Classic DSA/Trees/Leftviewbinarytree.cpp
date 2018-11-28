17d31cb50e0e84dc76cf7c783b5d4155b0157235
abhimanyu891998 <singhabhimanyu186@gmail.com>
2018-11-28 15:25:28 +0800

    Binary tree left view

---
+++ Classic DSA/Trees/Leftviewbinarytree.cpp
@@ -0,0 +1,111 @@
+{
+#include <bits/stdc++.h>
+using namespace std;
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+// A wrapper over leftViewUtil()
+void leftView(struct Node *root);
+/* Helper function that allocates a new node with the
+   given data and NULL left and right pointers. */
+struct Node* newNode(int data)
+{
+  struct Node* node = (struct Node*)
+                       malloc(sizeof(struct Node));
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+  return(node);
+}
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  struct Node *child;
+  scanf("%d
+", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d
+",&n);
+     struct Node *root = NULL;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+        if (m.find(n1) == m.end())
+        {
+           parent = newNode(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        child = newNode(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     leftView(root);
+     cout << endl;
+  }
+  return 0;
+}
+
+}
+#include<queue>
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* A binary tree node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+// A wrapper over leftViewUtil()
+
+void leftView(Node *root)
+{
+   // Your code here
+   
+   
+   if(root==NULL)return;
+   else{
+       queue<Node *>q;
+       q.push(root);
+       
+       Node * back = NULL;
+       while(!q.empty()){
+           Node * temp = q.front();
+           if(back==NULL)
+           {
+               cout<<temp->data<<" ";
+               q.push(NULL);
+           }
+           if(temp!=NULL){
+           if(temp->left!=NULL)
+           q.push(temp->left);
+           if(temp->right!=NULL)
+           q.push(temp->right);
+             }
+         back=q.front();
+        q.pop();
+       }
+       
+   }
+   
+}
